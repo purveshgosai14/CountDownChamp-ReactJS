66dd2b64a38a942d4a55ea3392d1c75edc3efb9d
Purveshpuri Gosai <puri.ui369@gmail.com>
2017-07-25 00:43:03 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'Dec 25, 2017',
+            deadline: 'Dec 31, 2017',
             newDeadline: ''
         }
     }
